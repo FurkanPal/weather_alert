31d7b77252b8334d43b891927d69a533dc8d4444
Ata <fpala74@gmail.com>
2024-02-29 12:00:00 +0100

    First commit

---
+++ day35/main.py
@@ -0,0 +1,36 @@
+import requests
+from twilio.rest import Client
+import os
+
+OWM_endpoints = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = os.environ.get("api_key")
+weather_params = {
+    "lat": 51.759050,
+    "lon": 19.458600,
+    "appid": api_key,
+    "exclude": "current, minutely, daily"
+}
+will_rain = False
+# Twillio
+account_sid = "[]"
+auth_token = os.environ.get("token")
+
+# Weather
+response = requests.get(OWM_endpoints, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+wearher_slice = weather_data["hourly"][0:12]
+
+for hourly in wearher_slice:
+    condition_code = hourly["weather"][0]["id"]
+    if condition_code > 700:
+        will_rain = True
+
+if will_rain == True:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="[]",
+        from_="[]",
+        to="[]"
+    )
+    print(message.status)
